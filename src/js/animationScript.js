cf6b44e4bed87b6778700983f5c85a67f5230520
Alina Gorbunova <alinagorbunova98@mail.ru>
2021-10-20 14:01:04 +0500

    Init commit

---
+++ src/js/animationScript.js
@@ -0,0 +1,12 @@
+'use strict';
+
+import {
+	isFullyVisible,
+	isPartiallyVisible,
+	setSteppedAnimation,
+	setAnimation,
+} from './animationFunctions.js';
+
+export function animate() {
+	const doc = document;
+}
---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { animate } from './animationScript.js';
+
+const doc = document;
+
+doc.addEventListener('DOMContentLoaded', function () {
+	animate();
+});
