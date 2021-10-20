d9743169e36e40c1085e60d1e82cef2116548b04
Alina Gorbunova <alinagorbunova98@mail.ru>
2021-10-20 18:16:03 +0500

    Add menu content and links

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,23 @@ import { animate } from './animationScript.js';
 
 const doc = document;
 
+const hamburger = doc.querySelector('.hamburger');
+
 doc.addEventListener('DOMContentLoaded', function () {
 	animate();
+
+	hamburger.addEventListener('click', openMenu);
 });
+
+function openMenu() {
+	const content = doc.querySelector('.content'),
+		sidePanel = doc.querySelector('.sidepanel');
+
+	if (hamburger.classList.contains('hamburger_up')) {
+		window.scrollTo(0, 0);
+	} else {
+		content.classList.toggle('content_folded');
+		sidePanel.classList.toggle('sidepanel_hidden');
+		hamburger.classList.toggle('active');
+	}
+}
